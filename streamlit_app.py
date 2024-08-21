2806267e5f160a346603338402db024c83ac5211
Allotoast <toastedtending@gmail.com>
2024-08-21 17:24:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Food Giver")
+st.title("Robin Hood")
 st.write("Lets feed the poor!")
-st.write("Collect food crates, Give to the poor, avoid the rich")
+st.write("Collect food crates, Give to the poor, avoid the rich from stealing the food for themselves")
